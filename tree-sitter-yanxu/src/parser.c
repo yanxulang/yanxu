89a9ceb173a4164a6b32c426d3abc68a6a23f206
刘秀 <xiu@srcmax.com>
2026-07-17 08:49:18 +0800

    发布：准备言序 1.1.13

--- tree-sitter-yanxu/src/parser.c
+++ tree-sitter-yanxu/src/parser.c
@@ -19188,7 +19188,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_yanxu(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 1,
-      .patch_version = 12,
+      .patch_version = 13,
     },
   };
   return &language;
